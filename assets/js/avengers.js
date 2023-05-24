77d3d72c8ff937322bb2a7b73d8ba46f078b9128
Ashwin-Dev-P <ashwindev1462001@gmail.com>
2023-05-24 23:42:44 +0530

    UPDATE. Mobile responsive

--- assets/js/avengers.js
+++ assets/js/avengers.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const mainTag = document.querySelector("main");
+
 fetch("./infinity_stones.json")
 	.then((response) => {
 		if (!response.ok) {
@@ -8,31 +10,29 @@ fetch("./infinity_stones.json")
 		return response.json();
 	})
 	.then((json) => {
-		console.log(json);
 		const { stones } = json;
 
 		let infinity_stones_ul = document.createElement("ul");
 		infinity_stones_ul.className = "infinity-stones-ul my-text-center";
 
 		for (const stone of stones) {
-			console.log(stone);
 			const { name, color } = stone;
 
 			const infinity_stone_list_item = document.createElement("li");
 			infinity_stone_list_item.innerText = name;
 			infinity_stone_list_item.style.backgroundColor = color;
 
-			console.log(infinity_stone_list_item);
 			infinity_stones_ul.appendChild(infinity_stone_list_item);
 		}
-
-		console.log(infinity_stones_ul);
-
-		const mainTag = document.querySelector("main");
-		console.log(mainTag);
 
 		mainTag.appendChild(infinity_stones_ul);
 	})
 	.catch(function (error) {
 		console.error("error", error);
+		const errorDialogP = document.createElement("dialog");
+		errorDialogP.className = "my-danger  my-text-center";
+		errorDialogP.innerText = "Unable to get the stone details";
+		errorDialogP.setAttribute("open", true);
+
+		mainTag.appendChild(errorDialogP);
 	});
