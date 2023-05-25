55cea3def21baa6b927ddf91816fa469ff1ca982
Ashwin-Dev-P <ashwindev1462001@gmail.com>
2023-05-25 12:44:48 +0530

    UPDATE. infinity stone with images

--- assets/js/avengers.js
+++ assets/js/avengers.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// stones image source: https://opensea.io/collection/infinity-stones
 const mainTag = document.querySelector("main");
 
 fetch("./infinity_stones.json")
@@ -12,15 +12,36 @@ fetch("./infinity_stones.json")
 	.then((json) => {
 		const { stones } = json;
 
-		let infinity_stones_ul = document.createElement("ul");
-		infinity_stones_ul.className = "infinity-stones-ul my-text-center";
+		// To append multiple children
+		// var documentFragment = document.createDocumentFragment();
+		// documentFragment.appendChild(infinity_stones_ul);
+
+		var infinity_stones_ul = document.createElement("ul");
+		infinity_stones_ul.className =
+			"infinity-stones-ul stones-flex-container my-text-center";
 
 		for (const stone of stones) {
 			const { name, color } = stone;
 
 			const infinity_stone_list_item = document.createElement("li");
-			infinity_stone_list_item.innerText = name;
-			infinity_stone_list_item.style.backgroundColor = color;
+
+			const image_div = document.createElement("div");
+
+			const stone_img = document.createElement("img");
+
+			stone_img.src = `./assets/images/stones/${name}.webp`;
+			stone_img.alt = `${name} stone`;
+			stone_img.width = "100%";
+
+			image_div.appendChild(stone_img);
+			infinity_stone_list_item.appendChild(image_div);
+
+			//const text_div = document.createTextNode(name);
+			const text_div = document.createElement("div");
+			text_div.innerHTML = name;
+			text_div.style.color = color;
+
+			infinity_stone_list_item.appendChild(text_div);
 
 			infinity_stones_ul.appendChild(infinity_stone_list_item);
 		}
